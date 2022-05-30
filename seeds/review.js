de2f8efbcef591775c562d9bf9c9b61fe28d5bb9
Rob.S <Rob.saba46@gmail.com>
2022-05-30 21:31:30 +1000

    update to review and movie seeds

--- seeds/review.js
+++ seeds/review.js
@@ -1,27 +1,48 @@
 const { Review } = require('../models/review');
 
 const reviewdata = [
-  {
-  id: 1,
-  user_id: 1,
-  movie_id: 1,
-  body: "good movie",
-  rating: 4,
+  {  
+    user_id: 1,
+    movie_id: 1,
+    body: 'good movie',
+    rating: 4,
   },
   {
-    id: 2,
     user_id: 2,
-    movie_id: 2,
-    body: "good movie",
+    movie_id: 1,
+    body: 'ok movie',
+    rating: 3,
+  },
+  {
+    user_id: 2,
+    movie_id: 3,
+    body: 'great movie',
+    rating: 5,
+  },
+  {
+    user_id: 3,
+    movie_id: 3,
+    body: 'good movie',
     rating: 4,
-    },
-    {
-        id: 2,
-        user_id: 2,
-        movie_id: 2,
-        body: "good movie",
-        rating: 4,
-        },
+  },
+  {
+    user_id: 4,
+    movie_id: 2,
+    body: 'ok movie',
+    rating: 3,
+  },
+  {
+    user_id: 5,
+    movie_id: 2,
+    body: 'great movie',
+    rating: 5,
+  },
+  {
+    user_id: 6,
+    movie_id: 2,
+    body: 'good movie',
+    rating: 4,
+  }
 
 ];
 
