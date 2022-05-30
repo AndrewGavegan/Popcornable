bfc41ac26365f10282d3434561111dd8ed957c7b
Rob.S <Rob.saba46@gmail.com>
2022-05-30 18:57:56 +1000

    update to models and seeds

--- models/Movies.js
+++ models/Movies.js
@@ -2,33 +2,33 @@ const { Model, DataTypes } = require('sequelize');
 const sequelize = require('../config/connection');
 
 Movie.init(
-    {
-      id: {
-        type: DataTypes.INTEGER,
-        allowNull: false,
-        primaryKey: true,
-        autoIncrement: true,
-      },
-      name: {
-        type: DataTypes.STRING,
-        allowNull: false,
-      },
-      year: {
-          type: DataTypes.STRING,
-          allowNull: false,
-      } ,
-      genre: {
-        type: DataTypes.STRING,
-        allowNull: false,
-      },
-      runtime: {
-          type: DataTypes.INTEGER,
-          allowNull: false
-      },
-      director: {
-          type: DataTypes.STRING,
-          allowNull: false
-      }
-    });
+{
+  id: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    primaryKey: true,
+    autoIncrement: true,
+  },
+  name: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  year: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  } ,
+  genre: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  runtime: {
+    type: DataTypes.INTEGER,
+    allowNull: false
+  },
+  director: {
+    type: DataTypes.STRING,
+    allowNull: false
+  }
+});
 
-    module.exports = Movie;
+module.exports = Movie;
