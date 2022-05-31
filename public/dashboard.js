9dd0899bbdd6ef1dee8bc411ce61ebf67f67ef9d
Andrew Gavegan <zzandrewzz@optusnet.com.au>
2022-05-31 21:10:06 +1000

    populated review.js event handler

--- public/dashboard.js
+++ public/dashboard.js
@@ -1,15 +1,15 @@
 // vanilla javascript for dashboard.handlebars where you view reviews for a particular movie //
-const newReview = async (e) => {
+const newMovie = async (e) => {
   e.preventDefault();
 
-  // grab movie name from wherever it is displayed in the handlebars files //
+  // grab id and movie name from wherever it is displayed in the handlebars files //
   const movieID = document.querySelector('').value.trim();
-  const reviewBody = document.querySelector('').value.trim();
+  const movieName = document.querySelector('').value.trim();
 
-  if (movieID && reviewBody) {
-    const res = await fetch('/api/review/post', {
-      method: 'POST',
-      body: JSON.stringify({ movieID, reviewBody }),
+  if (movieID && MovieBody) {
+    const res = await fetch('/api/Movie/GET', {
+      method: 'GET',
+      body: JSON.stringify({ movieID, movieName }),
       headers: { 'Content-Type': 'application/json' },
     });
 
@@ -19,5 +19,5 @@ const newReview = async (e) => {
   }
 };
 
-// needs to link to the button for creating a new review under the movie //
-document.querySelector('').addEventListener('click', newReview);
+// needs to link to the button for creating a new Movie under the movie //
+document.querySelector('').addEventListener('click', newMovie);
