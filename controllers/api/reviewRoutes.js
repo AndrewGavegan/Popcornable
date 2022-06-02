db13f654479b98bd8a45b8272dcd028ceb44f127
Rob.S <Rob.saba46@gmail.com>
2022-06-02 20:32:30 +1000

    review routes update

--- controllers/api/reviewRoutes.js
+++ controllers/api/reviewRoutes.js
@@ -1,63 +1,5 @@
 const router = require('express').Router();
-const { Movie, Review } = require('../../models');
-
-// find all movies
-router.get('/', async (req, res) => {
-  try{
-    const movieData = await Movie.findAll({
-      include: [
-        {
-        model: Movie,
-        attributes: ['name', 'description', 'image_url'],
-        },
-      ],
-    });
-    const movies = movieData.map((movie) =>
-      movie.get({ plain: true })
-    );
-    console.log(movies);
-    res.render('homepage', {
-      movies,
-      loggedIn: req.session.loggedIn,
-    }); 
-  }catch (err) {
-    console.log(err);
-    res.status(500).json(err);
-  }
-});
-
-
-// find one movie
-router.get('/movie/:id', async (req, res) => {
-  try {
-    const movieData = await Movie.findByPk (req.params.id, {
-      include: [
-        {
-          model: Movie,
-          attributes: [
-            'id',
-            'name',
-            'year',
-            'genre',
-            'runtime',
-            'director',
-            'image_url',
-          ],
-        },
-      ],
-    });
-    const movie = movieData.get({ plain: true });
-    console.log(movie);
-    res.render('homepage', {
-      movie,
-      loggedIn: req.session.loggedIn,
-    }); 
-    res.render()
-  }catch (err) {
-    console.log(err);
-    res.status(500).json(err);
-  }
-});
+const { Review } = require('../../models');
 
 // // find all reviews
 router.get('/', async (req, res) => {
@@ -89,11 +31,6 @@ router.post('/reviews', async (req,res) => {
   const reviewData = await Review.create({
   body: req.body.body,
   rating: req.body.rating,
-  
-  // like his example
-  // username: req.body.username,
-  // email: req.body.email,
-  // password: req.body.password,
 });
 
 req.session.save(() =>{
@@ -101,10 +38,15 @@ req.session.save(() =>{
   res.status(200).json(reviewData)
 }) 
 
+if (!reviewData){
+  res.status(400).json ({ message: 'Add a new review or rating!'})
+}
+
   }catch (err) {
     console.log(err);
     res.status(500).json(err);
   }
 }) 
+
 
 module.exports = router;
