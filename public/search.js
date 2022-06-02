d6f248343ed3367ba126d866187fcc49c568c79e
Andrew Gavegan <zzandrewzz@optusnet.com.au>
2022-06-02 20:43:12 +1000

    autocomplete dropdown javascript, without select option

---
+++ public/search.js
@@ -0,0 +1,31 @@
+document.querySelector('#searchBar') = searchBar
+document.querySelector('#autoComplete') = autoList
+
+const movieSearch = async userInput => {
+  const res = await fetch('../movieApi/assets/json/allMovies.json');
+  const results = await res.json();
+  let matches = results.filter(match => {
+    const compare = new RegExp(`^${userInput}`, 'gi');
+    return match.name.match(compare) || match.director.match(compare);
+  });
+
+  if (userInput.length === 0) {
+    matches = [];
+    autoList.innerHTML = '';
+  }
+
+  renderHTML(matches);
+};
+
+const renderHTML = matches => {
+  if (matches.length > 0) {
+    const html = matches.map(match => `
+    <div class='card card-body mb-1'>
+      <h4>${match.name} (${match.director})</h4>
+    </div>
+    `).join('');
+    autoList.innerHTML = html;
+  }
+}
+
+searchBar.addEventListener('input', () => movieSearch(search.value))
