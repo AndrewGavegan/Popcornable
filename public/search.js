8ee5ec484d804d159d4fabb9671230c7f80828ec
Andrew Gavegan <zzandrewzz@optusnet.com.au>
2022-06-05 11:39:15 +1000

    added functionality for view movie button on front page

--- public/search.js
+++ public/search.js
@@ -5,5 +5,14 @@ searchBtn.addEventListener('click', async function (event) {
   event.preventDefault();
   res = await userInput.value.toLowerCase().split(' ');
   console.log(res);
-  req = await fetch(`/api/movies/${res.join('')}`);
+  req = await fetch(`/api/movie/${res.join('')}`);
 });
+
+
+const viewBtn = document.querySelector('#viewBtn');
+
+viewBtn.addEventListener('click', async function (e) {
+  e.preventDefault();
+  const movie_id = e.target.getAttribute('data-movieid')
+  document.location.replace(`/api/movie/${movie_id}`);
+})
