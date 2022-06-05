b85fa4aa5a61fec266cb803ccba9226324be0935
AN <99376802+alainatividad@users.noreply.github.com>
2022-06-05 15:43:26 +1000

    Fixed search button function

--- public/search.js
+++ public/search.js
@@ -3,15 +3,27 @@ const searchBtn = document.querySelector('#submitBtn');
 
 searchBtn.addEventListener('click', async function (event) {
   event.preventDefault();
-  res = await userInput.value.toLowerCase().split(' ');
-  console.log(res);
-  req = await fetch(`/api/movie/${res.join('')}`);
+  res = await userInput.value;
+
+  let movieId;
+  const dataOptions = document.querySelectorAll('option');
+  dataOptions.forEach(element => {
+    if (element.getAttribute('value') === res) {
+      movieId = element.getAttribute('data-movieid');
+    }
+  });
+
+  if (movieId) {
+    document.location.assign(`/api/movie/${movieId}`);
+  } else {
+    alert('Sorry, the movie is not in the database (yet)');
+  }
 });
 
 const container = document.querySelector('#viewMovies');
 container.addEventListener('click', async function (e) {
   e.preventDefault();
-  let movie_id = e.target.getAttribute('data-movieid')
+  let movie_id = e.target.getAttribute('data-movieid');
   console.log(movie_id);
-  document.location.replace(`/api/movie/${movie_id}`);
-})
+  document.location.assign(`/api/movie/${movie_id}`);
+});
